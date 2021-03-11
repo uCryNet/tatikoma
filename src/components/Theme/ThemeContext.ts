d677ed93729ac37601ffdfa58de862d5f2c05318
CryNet <ucrynet@gmail.com>
2021-03-11 20:31:55 +0200

    feat: for gh-page

--- src/components/Theme/ThemeContext.ts
+++
@@ -1,7 +0,0 @@
-import {createContext} from 'react';
-import {ThemeContextValue} from './ThemeProvider'
-
-export const ThemeContext = createContext<ThemeContextValue>({
-    theme: () => {},
-    isDarkTheme: false,
-});
